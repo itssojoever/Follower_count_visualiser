c1393da4ad5a9bbdcc973cc196b5619cb0eee2d0
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-03-20 18:42:16 +0000

    Provisional configuration set up

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
-import requests
 import lxml
+import json
+import os
+from apscheduler.schedulers.background import BlockingScheduler
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 
-firefoxOptions = Options()
-firefoxOptions.add_argument("--headless")
-url = input("Please input desired site : ")
-driver = webdriver.Firefox(options=firefoxOptions)
-driver.get(url)
+def scheduler():
+    if os.path.isfile("config.json"):
+        print("Config found")
+        with open ("config.json") as f:
+            try:
+                data = json.load(f)
+                timeH = data.get("hour")
+                timeM = data.get("minutes")
+                scheduler = BlockingScheduler()
+                job_1 = scheduler.add_job(scrapeData, hour = timeH , minute= timeM)
+                scheduler.start()
+            except json.JSONDecodeError:
+                print ("Config file exists but is not complete, please relaunch config")
 
-resultOfRequest = BeautifulSoup(driver.page_source, "lxml")
+    
 
-metaTag = resultOfRequest.find("meta", attrs={"name" : "description"})
-content = metaTag["content"]
+def scrapeData():
 
-followerInfo = content.split("Followers,")[0].strip()
-followerCount = followerInfo.split()[0].replace(",", "")
+    firefoxOptions = Options()
+    firefoxOptions.add_argument("--headless")
+    url = input("Please input desired site : ")
+    driver = webdriver.Firefox(options=firefoxOptions)
+    driver.get(url)
 
-print(followerCount)
+    resultOfRequest = BeautifulSoup(driver.page_source, "lxml")
 
+    metaTag = resultOfRequest.find("meta", attrs={"name" : "description"})
+    content = metaTag["content"]
 
-#if resultOfRequest.status_code == 200:
-#    result = BeautifulSoup(resultOfRequest.content, "lxml")
-#    followerCount = result.find("span", class_="_ac2a")
-#    print(followerCount)
-#else:
-#    print ("Error with status code: ", resultOfRequest.status_code)
+    followerInfo = content.split("Followers,")[0].strip()
+    followerCount = followerInfo.split()[0].replace(",", "")
+
+    print(followerCount)
