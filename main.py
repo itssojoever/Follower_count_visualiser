040c311f907a0ba42b75ef6704e7729c1533fcb4
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-03-20 04:10:46 +0000

    main

---
+++ main.py
@@ -0,0 +1,4 @@
+'''
+This is an attempt to build a working visualiser for follower counts on a social media of my choosing. In my (in)experience, I think I will need to
+learn to use, to a certain extent, matlab, beautiful soup, and I'm tempted to use TKinter or another GUI library to set up a config.
+'''
