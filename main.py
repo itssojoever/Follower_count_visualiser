c61357baed2dc1e3e8ab59685d680ad30b876ffa
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-03-20 05:33:01 +0000

    Successfully scrapes follower count

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import requests
+import lxml
+from selenium import webdriver
 from bs4 import BeautifulSoup
+
+url = input("Please input desired site : ")
+driver = webdriver.Firefox()
+driver.get(url)
+
+resultOfRequest = BeautifulSoup(driver.page_source, "lxml")
+
+metaTag = resultOfRequest.find("meta", attrs={"name" : "description"})
+content = metaTag["content"]
+
+followerInfo = content.split("Followers,")[0].strip()
+followerCount = followerInfo.split()[0].replace(",", "")
+
+print(followerCount)
+
+
+#if resultOfRequest.status_code == 200:
+#    result = BeautifulSoup(resultOfRequest.content, "lxml")
+#    followerCount = result.find("span", class_="_ac2a")
+#    print(followerCount)
+#else:
+#    print ("Error with status code: ", resultOfRequest.status_code)
