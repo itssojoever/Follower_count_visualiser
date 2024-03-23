f7808eda3e070e86eec0c32d8bf053e2277b49f4
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-03-23 10:29:58 +0000

    Committing latest learning

--- main.py
+++ main.py
@@ -9,14 +9,31 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
+from collections import Counter
 
 #plt.style.use("fivethirtyeight")
+plt.xkcd()
 
 with open("data.csv") as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    row= next(csv_reader)
-    print(row["LanguagesWorkedWith"].split(";"))
 
+    language_counter = Counter()
+
+    for row in csv_reader:
+        language_counter.update(row["LanguagesWorkedWith"].split(";"))
+
+languages = []
+utilisation = []
+
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    utilisation.append(item[1])
+
+plt.barh(languages, utilisation)
+plt.title("Most popular languages")
+plt.xlabel("Programming languages")
+plt.ylabel("Number of users self-reported")
+plt.show()
 # #print(plt.style.available)
 # #plt.xkcd()
 
